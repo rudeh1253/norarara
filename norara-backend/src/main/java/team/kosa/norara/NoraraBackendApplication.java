873f8696aec4f5d0397d78c05130b2867b0a63c6
pgd <rudeh1253@gmail.com>
2025-02-25 18:27:24 +0900

    chore: initialize project

---
+++ norara-backend/src/main/java/team/kosa/norara/NoraraBackendApplication.java
@@ -0,0 +1,13 @@
+package team.kosa.norara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoraraBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoraraBackendApplication.class, args);
+	}
+
+}
---
+++ norara-backend/src/test/java/team/kosa/norara/NoraraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package team.kosa.norara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoraraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
